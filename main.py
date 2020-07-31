f388911444c56cf86eb82c864f5b2efc43f808e0
Jaehyuk Heo <wogur379@gmail.com>
2020-08-01 02:37:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 import os 
 from comments import *
 
-def run(ha_id, ha_pwd, token_v2, ha_notion):
-    driver = get_board_list(ha_id, ha_pwd)
-    boards_info, driver = get_board_urls(driver)
-    total_df = get_comment_urls(driver, boards_info)
-    
-    print('[COMPLETE] Number of boards with comments: ',total_df.shape[0])
-    
-    add_notion(token_v2, ha_notion, total_df)
-    print('[COMPLETE] Create a table in Notion')
-
-    return True
 
 if __name__=='__main__':
 
@@ -20,6 +9,11 @@ if __name__=='__main__':
     token_v2 = os.environ['TOKEN_V2']
     ha_notion = os.environ['HA_NOTION']
 
-    run(ha_id, ha_pwd, token_v2, ha_notion)
+    driver = get_board_list(ha_id, ha_pwd)
+    boards_info, driver = get_board_urls(driver)
+    total_df = get_comment_urls(driver, boards_info)
+    print('[COMPLETE] Number of boards with comments: ',total_df.shape[0])
+    add_notion(token_v2, ha_notion, total_df)
+    print('[COMPLETE] Create a table in Notion')
 
     
